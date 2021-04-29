8f6a4ff666c60f13ed6d8dc960223cc0cb513a23
naqaden <naqaden@users.noreply.github.com>
2021-04-29 22:45:15 +0000

    Only show on tooltips with atkspd
    AAS tooltips were showing up on items that don't have atkspd in their tooltips.

--- src/main/java/by/jackraidenph/aas/ActualAttackSpeedMod.java
+++ src/main/java/by/jackraidenph/aas/ActualAttackSpeedMod.java
@@ -4,7 +4,6 @@ import com.google.common.collect.Multimap;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -23,8 +22,6 @@ public class ActualAttackSpeedMod {
     public static final String VERSION = "1.1.0";
     public static Logger MOD_LOGGER;
     private DecimalFormat df2 = new DecimalFormat("#.###");
-    private int index;
-    private String original;
 
     private double sumAllAndReturn(Collection<AttributeModifier> col) {
         return col.stream().map(AttributeModifier::getAmount).reduce(0.0D, Double::sum);
@@ -38,20 +35,17 @@ public class ActualAttackSpeedMod {
 
     @SubscribeEvent
     public void onTooltip(ItemTooltipEvent e) {
-        e.getToolTip().forEach(x -> {
-            if (x.contains(I18n.format("attribute.name.generic.attackSpeed"))) {
-                index = e.getToolTip().indexOf(x);
-                original = x;
+        for (String tipLine : e.getToolTip()) {
+            if (tipLine.contains(I18n.format("attribute.name.generic.attackSpeed"))) {
+                int index = e.getToolTip().indexOf(tipLine);
+                Multimap<String, AttributeModifier> map = e.getItemStack().getAttributeModifiers(EntityEquipmentSlot.MAINHAND);
+                double attackSpeed = 4 + sumAllAndReturn(map.get("generic.attackSpeed"));
+                double damage = 1 + sumAllAndReturn(map.get("generic.attackDamage"));
+                double actualSpeedInSeconds = Math.ceil((20 / attackSpeed)) / 20;
+                e.getToolTip().set(index, " " + I18n.format("cooldown.time", df2.format(actualSpeedInSeconds).trim()) + " (" + tipLine.trim() + ")");
+                e.getToolTip().add(index, " " + I18n.format("cooldown.dps", df2.format(damage / actualSpeedInSeconds)));
+                break;
             }
-        });
-        try {
-            Multimap<String, AttributeModifier> map = e.getItemStack().getAttributeModifiers(EntityEquipmentSlot.MAINHAND);
-            double attackSpeed = 4 + sumAllAndReturn(map.get("generic.attackSpeed"));
-            double damage = 1 + sumAllAndReturn(map.get("generic.attackDamage"));
-            double actualSpeedInSeconds = Math.ceil((20 / attackSpeed)) / 20;
-            e.getToolTip().set(index, " " + I18n.format("cooldown.time", df2.format(actualSpeedInSeconds).trim()) + " (" + original.trim() + ")");
-            e.getToolTip().add(index, " " + I18n.format("cooldown.dps", df2.format(damage / actualSpeedInSeconds)));
-        } catch (Exception ignored) {
         }
     }
 }
